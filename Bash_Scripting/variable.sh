1ad296199687f83da5325c559e1853948c725f10
rhoda-lee <rhodalee.dev@gmail.com>
2024-10-16 17:06:54 +0000

    variables in bash

---
+++ Bash_Scripting/variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name="Rhoda Lee"
+program="Tech4Girls"
+region="Greater Accra Region"
+hobby="read and write"
+num_of_siblings=3
+laptop_name=$NAME
+
+echo " Hello my name is $name! Nice to meet you
+I am a $program scholar and I live in a town in the $region. 
+I love to $hobby when I am at home. I have $num_of_siblings other siblings. The name of my laptop is $laptop_name"
+
