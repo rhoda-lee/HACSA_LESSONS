a80ce272249b968407f54760ec6afce72768f067
rhoda-lee <rhodalee.dev@gmail.com>
2024-10-16 17:30:05 +0000

    Lesson on bash scripting

--- Bash_Scripting/read.sh
+++ Bash_Scripting/read.sh
@@ -7,6 +7,8 @@ read name
 echo "How old are you? "
 read age
 
+
+
 echo "What is your password?"
 read -sp password: 
 
@@ -14,5 +16,14 @@ read -sp password:
 name="Dede"
 
 my_fortune=$(fortune)
+
+talking_cow=$(cowsay "Hii")
+
 echo "Hello $name, you are $age years old! 
-That is too old! Anyway, your password has been saved. And your fortune is: $my_fortune"
+That is too old! Anyway, your password has been saved. 
+And your fortune is: $my_fortune 
+And the cow said $talking_cow"
+
+read -p "Enter your firstname and lastname" firstname lastname
+echo $firstname
+echo $lastname
