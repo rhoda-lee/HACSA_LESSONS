1b5a89b002747eaf06c27e8188c4a5c3ec612738
rhoda-lee <rhodalee.dev@gmail.com>
2024-10-25 16:39:19 +0000

    added code for lesson on tuple

---
+++ Python_Lessons/tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+my_tuple = (1, 3, 5, 7, 11, 13, 17, 19, 23)
+print(my_tuple)
+
+length = len(my_tuple)
+print(length)
+
+index = my_tuple.index(7)
+print(index)
+
+count = my_tuple.count(19)
+print(count)
