45ca513c192cc42c7a4f80ae0aed3b2749cd681a
rhoda-lee <rhodalee.dev@gmail.com>
2024-11-06 17:47:35 +0000

    run tests

--- Python_Lessons/test.py
+++ Python_Lessons/test.py
@@ -1,23 +1,172 @@
-n = int(input('Enter an integer: '))
+# Function to save user pin
+def save_pin():
+    global pin
+    while True:
+        pin = input('Add a 4-digit pin: ')
 
-if n % 2 == 0:
-    if n > 20:
-        print('Not Weird')
-    elif n >= 6 and n <= 20:
-        print('Weird')
-    elif n >= 2 and n <= 5:
-        print('Not Weird')
-else:
-    print('Weird')
-
-
-def swap_case(s):
-    updated_string = ""
-    for i in s:
-        if i.islower():
-            updated_string += i.upper()
-        elif i.isupper():
-            updated_string += i.lower()
+        if pin.isdigit() and len(pin) == 4:
+            print('Pin saved successfully.\n')
+            break
         else:
-            updated_string += i
-    return updated_string
+            print('Please enter a 4-digit pin.\n')
+
+
+# Function to change user pin
+def change_pin():
+    while True:
+        user_pin = input('Enter your current pin: ')
+
+        global pin
+
+        if user_pin == pin:
+            print('You are about to change your PIN')
+        else:
+            print('Wrong PIN')
+        
+        new_pin = input('Enter a new 4-digit pin: ')
+        confirm_pin = input('Enter new PIN again: ')
+
+        if confirm_pin == new_pin:
+
+            if new_pin.isdigit() and len(new_pin) == 4:
+                pin = new_pin
+                print('Pin updated successfully!')
+                break
+            else:
+                print('Please enter a 4-digit pin.')
+
+        else:
+            print('This does not match up with the PIN you just entered')
+
+
+
+# User authentication function
+user_pin = ''
+def authenticate_user():
+    max_attempts = 3
+    attempts = 0
+    
+    while attempts < max_attempts:
+        user_pin = input('Enter your pin: ')
+
+        if user_pin == pin:
+            print('Authentication successful!')
+            return True
+        else:
+            attempts += 1
+            print('Incorrect pin. Try again')
+
+        if attempts == max_attempts:
+            print('Too many incorrect attempts. Access blocked.')
+            return False
+
+
+
+
+
+
+
+#---------------------------------- Antwiwaa
+
+depositors_balance = 0
+#Function to check Balance   
+def check_balance():
+    global depositors_balance
+    authenticate_user()
+    print(f"Current Balance: ${depositors_balance:.2f}\n")
+
+# User deposit
+def deposit_funds():
+    global depositors_balance
+
+    print(f'Your account is ${depositors_balance:.2f}\n')
+    amount = float(input('Enter an amount to deposit: '))
+    
+    if amount > 0:
+        print('Enter your pin to proceed')
+        authenticate_user()
+        depositors_balance += amount
+        print(f'Successfully deposited ${amount:.2f}\nCrrent Balance: ${depositors_balance:.2f}\n')  
+    else:
+        print('Enter a non-negative amount to deposit') 
+
+
+#Withdraw funds
+withdraw: float    
+def withdraw_funds():
+    global depositors_balance
+    withdraw = float(input('Enter withdrawal amount: '))
+    if withdraw > depositors_balance:
+        print('Insufficient Balance')
+        withdraw_funds()
+    elif withdraw <= 0:
+        print('Enter a non-negative amout to withdraw') 
+        withdraw_funds()  
+    else:
+        print('Enter your pin to proceed')
+        authenticate_user()
+        depositors_balance -= withdraw
+        print(f'An amount of ${withdraw:.2f} withdrawn successfullly\nCurrent balace is: ${depositors_balance:.2f}')  
+
+
+#-------------------------------------- Antwiwaa
+
+
+
+
+
+def main_menu():
+    while True:
+
+        print(
+        'Main Menu Options:\n'
+        '1. Check Balance\n'
+        '2. Deposit Funds\n'
+        '3. Withdraw Funds\n'
+        '4. Change PIN\n'
+        '5. Exit'
+    )
+        user_choice = input('Enter number associated with choice: ')
+
+        if user_choice == '1':
+            print('Enter your pin to proceed.')
+            check_balance()
+        elif user_choice == '2':
+            deposit_funds()
+        elif user_choice == '3':
+            withdraw_funds()
+        elif user_choice == '4':
+            change_pin()
+        elif user_choice == '5':
+            print('Exiting main menu\n'
+                  'Thank you for visiting Rholant ATM Services.\n'
+                  'We hope you come next time.'
+            )
+            break
+        else:
+            print('Invalid choice!')
+
+
+# Main Application
+def main():
+    print('Welcome to RholAnt Bank ATM Services.\n'
+          'Add a pin to proceed') 
+    save_pin()
+    print('Enter your new pin to proceed to the main menu.')
+    authenticate_user()
+    main_menu()
+
+main()
+  
+   
+        
+
+
+
+
+
+
+
+
+
+
