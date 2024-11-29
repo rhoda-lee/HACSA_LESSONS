db39b1c81c3c6e2646bdbde6bc6793d598e40bcb
rhoda-lee <rhodalee.dev@gmail.com>
2024-11-29 18:02:53 +0000

    Updated app.py

--- Databases/sql_alchemy/app.py
+++ Databases/sql_alchemy/app.py
@@ -1,66 +1,65 @@
 from flask import Flask, jsonify, request
+from backend_crud import BackendCrud
+from config import session
+from books import books_bp
 
 app = Flask(__name__)
+backend_crud = BackendCrud(session)
+app.register_blueprint(books_bp)
 
-# defining a list of dicitonaries
-books = [
-    {'id':1,
-     'title':'1984',
-     'author':'George Orwell'
-    },
-
-    {'id':2,
-     'title':'To Kill a Mocking Bird',
-     'author':'Harper Lee'
-    },
-
-    {'id':3,
-     'title':'YOU',
-     'author':'Caroline kepnes'
-    },
-]
 
 # Creating an endpoint with Flask use @route decorator: @app.route('/') where '/' is the home directory
 @app.route('/')
 def home():
     return 'Welcome to Flask'
 
-# Endpoint 2
-@app.route('/another_home')
-def another_home():
-    return 'This is just another Home'
 
-# books endpoint, specify methods to use
-@app.route('/books', methods = ['GET'])
-def get_all_books():
-    # function to make sure our response always comes in a JSON file
-    return jsonify(books), 200
-
-# endpoint to get a specific book
-@app.route('/books/<int:book_id>', methods = ['GET'])
-def get_book_by_id(book_id):
-    for book in books:
-        if book['id'] == book_id:
-            return jsonify(book), 200
-       
-    return jsonify({'error':'Book not found'}), 404
-
-# endpoint to get book based on author
-@app.route('/books/<string:author_name>', methods = ['GET'])
-def get_book_by_author(author_name):
-    for book in books:
-        if book['author'] == author_name:
-            return jsonify(book), 200
-        
-    return jsonify({'error': 'Author not found'}), 404
+@app.route('/students', methods = ['GET'])
+def get_all_students():
+    students = backend_crud.get_all_students()
+    students_list = []
+    for student in students:
+        students_list.append({
+            'id': student.student_id,
+            'firstname': student.first_name,
+            'lastname': student.last_name,
+            'email': student.email
+        })
+    return jsonify(students_list)
 
 
-# Making Post Requests, use request function, needs to be imported to get the body of a request
-@app.route('/books', methods = ['POST'])
-def add_book():
-    new_book = request.get_json()
-    books.append(new_book)
-    return jsonify({'New Book': new_book})
+@app.route('/students/<string:first_name>', methods = ['GET'])
+def get_by_firstname(first_name):
+    student = backend_crud.get_student_by_first_name(first_name)
+    return jsonify({
+        'id': student.student_id,
+        'firstname': student.first_name,
+        'lastname': student.last_name,
+        'email': student.email
+    })
+
+
+# POST request
+@app.route('/students', methods = ['POST'])
+def add_student():
+    details = request.get_json()
+    if details is not None:
+        new_student = backend_crud.insert_student(
+            first_name = details.get('first_name'),
+            last_name = details.get('last_name'),
+            email = details.get('email'),
+            age = details.get('age')
+        )
+        return jsonify({'New Student Added': {
+            'id': new_student.student_id,
+            'first_name': new_student.first_name,
+            'last_name': new_student.last_name,
+            'email': new_student.email
+        }})
+    else:
+        return jsonify({'Error': 'Bad request'}), 400
+
+
 
 # Have backend server run
 if __name__ == '__main__':
