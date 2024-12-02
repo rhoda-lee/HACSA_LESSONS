e8de0d5ded82af6dfc9a290eae4ee19af47cc96e
rhoda-lee <rhodalee.dev@gmail.com>
2024-12-02 18:03:14 +0000

    lessons on the basics of JavaScript

---
+++ JavaScript/basics.js
@@ -0,0 +1,27 @@
+console.log(4)
+console.log('A string')
+
+// Strict equal to sign
+console.log('hey' === 'HEY')
+
+// Control structure
+// if statements
+if (7 > 5){
+    console.log('7 is greater than 5')
+}
+else if (7 < 5) {
+    console.log('7 is less than 5')
+}
+else {
+    console.log('Whatever!')
+}
+
+
+if (true) {
+    let message = 'JS is awesome'
+    console.log(message)
+}
+
+//checking datatypes in JavaScript
+// typeof()
+console.log(typeof('Rhoda'))
